375aede8a8e40f30dc90d4ef3e90eb3cb1e69099
PiyushGhavghave <piyushghavghave@gmail.com>
2024-08-30 22:29:34 +0530

    fixed navigation

--- result/result.js
+++ result/result.js
@@ -24,7 +24,17 @@ const careerChart = new Chart(ctx, {
 });
 
 
+const hamburger = document.querySelector('.hamburger');
+const sidebar = document.getElementById('sidebar');
+
+hamburger.addEventListener('click', () => {
+    sidebar.classList.toggle('active');
+});
+
+
 
 document.getElementById('printButton').addEventListener('click', () => {
     window.print();  // Opens the print dialog
 });
+
+
